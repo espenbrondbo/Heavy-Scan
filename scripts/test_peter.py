22e89bb62d0b583f44ccbf5ebf42bb89ae138408
mephest0 <feter.no@gmail.com>
2017-04-18 15:56:53 +0200

    Added comments and minor changes

--- scripts/test_peter.py
+++ scripts/test_peter.py
@@ -1,20 +1,28 @@
 from heavy import *
 
-ips = [ip_test]
-senders = [None, "30.152.10.10"]
+#addresses to scan
+ips = ["209.85.128.0", "209.85.128.1", "209.85.128.2"]
 
-ports = []
-for port in range(1, 10):
-	ports.append(port)
+#addresses to make it look like scan came from (None is normal IP)
+senders = [None, "4.4.4.4"]
 
+#ports to scan
+ports = [21, 80, 441]
+
+#scan loop
 for port in ports:
 	for ip in ips:
 		for sender in senders:
-			thread.start_new_thread(null_scan, (ip, 22,))
+			#make scan look like its coming from somewhere else
 			if syn_scan(ip, port, sender=sender):
 				print " hit on \t", ip, "\t", port, "\tfrom", sender
+				if sender is None:
+					#only print header if scan was from this ip
+					get_header(ip, port)
 			else:
 				print " nothing on\t", ip, "\t", port, "\tfrom", sender
 
 print "+done sending"
+
+#wait for all requests to complete or time out
 time.sleep(4)
